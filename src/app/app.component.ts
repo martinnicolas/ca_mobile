dbeddd02e1b8ba067d0dca8ece062aa6ce28548c
martinnicolas <ttincho77@gmail.com>
2019-01-17 18:57:36 -0300

    Agrego cancel a search bar de reclamos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      //statusBar.styleDefault(); //Oculta la barra de estados (reloj, etc)
+      statusBar.styleLightContent();
       splashScreen.hide();
     });
   }
