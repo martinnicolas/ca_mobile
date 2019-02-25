3c2a89e241fd4afc036a81a708aa817c681e35c8
martinnicolas <ttincho77@gmail.com>
2019-02-25 00:06:21 -0300

    Cambio tamaño de mapas en form de reclamos

--- src/providers/api-rest-v1/BaseProvider.ts
+++ src/providers/api-rest-v1/BaseProvider.ts
@@ -1,4 +1,4 @@
 export abstract class BaseProvider{
-    protected baseURL = 'http://localhost:3000'; // URL to web api Rails
-    //protected baseURL = 'http://ciudadactiva.herokuapp.com'; // URL to web api Rails - Produccion
+    //protected baseURL = 'http://localhost:3000'; // URL to web api Rails
+    protected baseURL = 'http://ciudadactiva.herokuapp.com'; // URL to web api Rails - Produccion
 }
